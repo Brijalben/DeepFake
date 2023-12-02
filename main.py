8048ee620e785fb8cb1717376df98009f3f2163a
abhijeet-ds <91625000+abhijeet-ds@users.noreply.github.com>
2023-12-02 14:26:36 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
+import torch
+from torchvision import transforms
+from PIL import Image
+import io
 import dill
 from flask import Flask, request, jsonify, render_template
+from werkzeug.exceptions import BadRequestKeyError
+
 
 app = Flask(__name__, template_folder='template')
 
 # Load the model
 with open('model.pkl', 'rb') as file:
     model = dill.load(file)
+    
+# Define the image preprocessing pipeline
+transform = transforms.Compose([ transforms.Resize((224, 224)),
+                                transforms.ToTensor(),
+                                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+def process_image(file):
+    # Open the image using PIL
+    image = Image.open(io.BytesIO(file.read()))
+    
+    # Preprocess the image
+    input_tensor = transform(image)
+    input_batch = input_tensor.unsqueeze(0)  # Add a batch dimension
+
+    return input_batch
+
 
 @app.route('/')
 def index():
@@ -13,17 +35,38 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get data from the request
-    data = request.files['image']
+    try:
+        # Get data from the request
+#         print(request.files)  # Debugging statement
+        data = request.files['image']
+        
+        
+         # Process the image
+        input_batch = process_image(data)
 
-    # Perform prediction using the loaded model
-    # Assuming your model expects image data, adjust this part accordingly
-    # For example, you might use a library like PIL to process the image
-    # before making predictions.
-    prediction = model.predict(data)
+        # Ensure the model is in evaluation mode
+        model.eval()
 
-    # Return the prediction as JSON
-    return jsonify({'prediction': prediction})
+        # Make predictions
+        with torch.no_grad():
+            _, output = model(input_batch)
 
-if _name_ == '_main_':
+        # Convert output to probabilities and get the predicted class
+#         probabilities = torch.nn.functional.softmax(output[0], dim=0)
+#         predicted_class = torch.argmax(probabilities).item()
+            predicted_class = torch.argmax(output, dim=1).cpu().numpy()
+
+        
+        if predicted_class == 0:
+            return "Image is real"
+        else:
+            return "Image is fake"
+        # Return the prediction as JSON
+#         return jsonify({'prediction': predicted_class}) #, 'probabilities': probabilities.tolist()})
+
+        
+    except BadRequestKeyError:
+        return 'Bad Request: Image file not provided or with incorrect key', 400
+
+if __name__ == '__main__':
     app.run(debug=True)
